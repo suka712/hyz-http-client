78a33233ef58e564356d40df6628d3d4df7c4eb8
khiem <khiemnguyen.hye@gmail.com>
2026-02-21 07:57:12 +0700

    GET endpoints without headers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::env;
+
 fn main() {
-    println!("Hello from hyz");
+    println!("Hello from hz");
+
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 3 {
+        eprintln!("Usage: hz <METHOD> <URL>");
+        std::process::exit(1);
+    }
+
+    let method = &args[1];
+    let url = &args[2];
+
+    let client = reqwest::blocking::Client::new();
+    let response = client.request(method.parse().unwrap(), url).send().unwrap();
+
+    println!("{:?} {}", response.version(), response.status());
+
+    for (name, value) in response.headers() {
+        println!("{}: {}", name, value.to_str().unwrap());
+    }
+
+    println!();
+    println!("{}", response.text().unwrap());
 }
